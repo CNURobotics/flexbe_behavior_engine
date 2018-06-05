c9d06c8373847414f715b64fa18c22fdc83812af
David C. Conner <david.conner@cnu.edu>
2018-06-05 16:27:59 -0400

    add enter wait duration parameter, allow late connection, and get_goal_status_text to the proxy action client

--- flexbe_core/src/flexbe_core/proxy/proxy_action_client.py
+++ flexbe_core/src/flexbe_core/proxy/proxy_action_client.py
@@ -5,6 +5,12 @@ import rospy
 import actionlib
 from threading import Timer
 import time
+import sys, traceback
+import os
+
+traceback_template = '''Traceback (most recent call last):
+  File "%(filename)s", line %(lineno)s, in %(name)s
+%(type)s: %(message)s\n''' # Skipping the "actual line" item
 
 from flexbe_core.logger import Logger
 
@@ -15,36 +21,49 @@ class ProxyActionClient(object):
     """
     _clients = {}
 
+    _msg_types = {}
     _result = {}
     _feedback = {}
 
-    def __init__(self, topics = {}):
+    def __init__(self, topics = {}, enter_wait_duration=0.0):
         """
         Initializes the proxy with optionally a given set of clients.
-        
-        @type topics: dictionary string - message class
-        @param topics: A dictionay containing a collection of topic - message type pairs.
+
+        @type topics              : dictionary
+        @param topics             : A dictionary containing a collection of topic - message type pairs.
+        @type wait_duration       : float
+        @param enter_wait_duration: Defines how long to wait for the given client if it is not available right now.
         """
+
+        # Try to connect on startup
+        wait_duration = 10 # default on startup
+        if (enter_wait_duration > 0):
+            wait_duration = enter_wait_duration
 
         for topic, msg_type in topics.iteritems():
-            self.setupClient(topic, msg_type)
-    
+            self._msg_types[topic] = msg_type
+            self.setupClient(topic, msg_type, wait_duration)
 
-    def setupClient(self, topic, msg_type, wait_duration=10):
+    def setupClient(self, topic, msg_type, wait_duration=10.):
         """
         Tries to set up an action client for calling it later.
-        
+
         @type topic: string
         @param topic: The topic of the action to call.
-        
+
         @type msg_type: msg type
         @param msg_type: The type of messages of this action client.
-        
-        @type wait_duration: int
+
+        @type wait_duration: float
         @param wait_duration: Defines how long to wait for the given client if it is not available right now.
         """
         if topic not in ProxyActionClient._clients:
             client = actionlib.SimpleActionClient(topic, msg_type)
+
+            # Register this client with proxy
+            ProxyActionClient._clients[topic] = client
+
+            # Attempt to validate the connection
             t = Timer(1, self._print_wait_warning, [topic])
             t.start()
             available = client.wait_for_server(rospy.Duration.from_sec(wait_duration))
@@ -56,29 +75,33 @@ class ProxyActionClient(object):
                 warning_sent = True
 
             if not available:
-                Logger.logerr("Action client %s timed out!" % topic)
+                Logger.logerr("Action client %s timed out before connection made!" % topic)
             else:
-                ProxyActionClient._clients[topic] = client
                 if warning_sent:
                     Logger.loginfo("Finally found action client %s..." % (topic))
-            
-            
+
+
     def send_goal(self, topic, goal):
         """
         Performs an action call on the given topic.
-        
+
         @type topic: string
         @param topic: The topic to call.
-        
+
         @type goal: action goal
         @param goal: The request to send to the action server.
         """
         if topic not in ProxyActionClient._clients:
-            raise ValueError('ProxyActionClient: topic %s not yet registered!' % topic)
-        
+            raise ValueError('ProxyActionClient - topic %s not yet registered!' % topic)
+
+        # Verify that there is an action server ready to process the goal
+        if not ProxyActionClient._clients[topic].wait_for_server(rospy.Duration.from_sec(0.0001)):
+            raise ValueError('ProxyActionClient - topic %s is not yet connected to server!' % topic)
+
+        # Clear prior results
         ProxyActionClient._result[topic] = None
         ProxyActionClient._feedback[topic] = None
-        
+
         ProxyActionClient._clients[topic].send_goal(goal,
             done_cb = lambda ts, r: self._done_callback(topic, ts, r),
             feedback_cb = lambda f: self._feedback_callback(topic, f)
@@ -94,7 +117,7 @@ class ProxyActionClient(object):
     def is_available(self, topic):
         """
         Checks if the client on the given action topic is available.
-        
+
         @type topic: string
         @param topic: The topic of interest.
         """
@@ -103,16 +126,33 @@ class ProxyActionClient(object):
     def has_result(self, topic):
         """
         Checks if the given action call already has a result.
-        
+
         @type topic: string
         @param topic: The topic of interest.
         """
         return ProxyActionClient._result[topic] is not None
 
+    def get_goal_status_text(self, topic):
+        """
+        Returns the result status text if any.
+
+        @type topic: string
+        @param topic: The topic of interest.
+        """
+
+        if (not topic in ProxyActionClient._clients):
+            return "Failed to get status text: Invalid action topic = [" + topic +"]"
+
+        try:
+            status = ProxyActionClient._clients[topic].get_goal_status_text()
+            return status
+        except:
+            return "Failed to retrieve goal status text for topic = [" + topic +"]"
+
     def get_result(self, topic):
         """
         Returns the result message of the given action call.
-        
+
         @type topic: string
         @param topic: The topic of interest.
         """
@@ -121,7 +161,7 @@ class ProxyActionClient(object):
     def has_feedback(self, topic):
         """
         Checks if the given action call has any feedback.
-        
+
         @type topic: string
         @param topic: The topic of interest.
         """
@@ -130,7 +170,7 @@ class ProxyActionClient(object):
     def get_feedback(self, topic):
         """
         Returns the latest feedback message of the given action call.
-        
+
         @type topic: string
         @param topic: The topic of interest.
         """
@@ -140,7 +180,7 @@ class ProxyActionClient(object):
         """
         Determines the current actionlib state of the given action topic.
         A list of possible states is defined in actionlib_msgs/GoalStatus.
-        
+
         @type topic: string
         @param topic: The topic of interest.
         """
@@ -149,7 +189,7 @@ class ProxyActionClient(object):
     def is_active(self, topic):
         """
         Determines if an action request is already being processed on the given topic.
-        
+
         @type topic: string
         @param topic: The topic of interest.
         """
@@ -158,7 +198,7 @@ class ProxyActionClient(object):
     def cancel(self, topic):
         """
         Cancels the current action call on the given action topic.
-        
+
         @type topic: string
         @param topic: The topic of interest.
         """
